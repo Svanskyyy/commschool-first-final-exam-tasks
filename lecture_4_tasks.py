7f2c357553a24bf20d2b4a854738b894ace5121e
Levan <levanliparteliani@gmail.com>
2026-07-16 23:03:05 +0400

    Add library task and initial setup

---
+++ lecture_4_tasks.py
@@ -0,0 +1,22 @@
+"""
+#1 დავალება
+
+def check_letter(letter):
+    ხმოვნები = "აეიოუ"
+    if letter in ხმოვნები:
+        print(f'"{letter}" ხმოვანია')
+    else:
+        print(f'"{letter}" თანხმოვანია')
+
+symbol = input("შემოიტანე სიმბოლო: ")
+check_letter(symbol)
+"""
+"""
+#2 დავალება
+
+for x in range(10, -1, -1):
+    print(x)
+""""
+
+#3 დავალება
+
